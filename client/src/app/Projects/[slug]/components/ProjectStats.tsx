9e1b7512db65c06235b2e206b979bea8f74fac8f
PolarDyth <williamalexander1603@gmail.com>
2025-03-08 01:52:14 +0000

    update ProjectStats component to change grid layout for better responsiveness

--- client/src/app/Projects/[slug]/components/ProjectStats.tsx
+++ client/src/app/Projects/[slug]/components/ProjectStats.tsx
@@ -12,7 +12,7 @@ export default function ProjectStats(prop: ProjectStatsProps) {
   return (
     <section id="stats">
       <div className="container mx-auto py-12">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 items-center justify-between">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 items-center justify-between">
           {prop.stats.map((stat, index) => (
             <div
               key={index}
